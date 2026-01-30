7bbce06f3afc97489c7e90d1b7de52b51495d38a
Athiphat Saramas <athiphatsaramas@gmail.com>
2026-01-31 01:29:39 +0700

    Finished MeleeUnit Initialization

--- src/main/java/unit/MeleeUnit.java
+++ src/main/java/unit/MeleeUnit.java
@@ -1,4 +1,9 @@
 package unit;
 
-public class MeleeUnit {
+public class MeleeUnit extends BaseUnit{
+    MeleeUnit(int startColumn, int startRow, boolean isWhite, String name) {
+        super(startColumn, startRow, isWhite, name);
+        super.hp = 5;
+        super.power = 2;
+    }
 }
