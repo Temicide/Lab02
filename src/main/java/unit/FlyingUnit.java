a0ffb95ae3517e6c3e20d0b61793b1728d094432
Athiphat Saramas <athiphatsaramas@gmail.com>
2026-01-31 01:38:46 +0700

    Finished FlyingUnit Initialization

---
+++ src/main/java/unit/FlyingUnit.java
@@ -0,0 +1,33 @@
+package unit;
+
+public class FlyingUnit extends BaseUnit{
+    public FlyingUnit(int startColumn, int startRow, boolean isWhite, String name) {
+        super(startColumn, startRow, isWhite, name);
+        super.hp = 2;
+        super.isFlying = true; 
+    }
+    
+    @Override
+    public boolean move(int direction) {
+        if (direction < 0 || direction > 3) return false;
+
+        if (direction == 0) {
+            if (super.getRow() >= 3) return false;
+            super.setRow(super.getRow() + 1);
+        }
+        else if (direction == 1) {
+            if (super.getColumn() >= 3) return false;
+            super.setColumn(super.getColumn() + 1);
+        }
+        else if (direction == 2) {
+            if (super.getRow() <= 1) return false;
+            super.setRow(super.getRow() - 1);
+        }
+        else {
+            if (super.getColumn() <= 1) return false;
+            super.setColumn(super.getColumn() - 1);
+        }
+
+        return true;
+    }
+}
